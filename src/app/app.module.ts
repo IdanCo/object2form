b28cdff6576d922ef4987aefa0397f4d1bb71661
Idan Cohen <idan@idancohen.com>
2017-10-11 17:45:37 +0300

    Add Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TransformerComponent } from './components/transformer/transformer.component';
+import { TransformerSettingsComponent } from './components/transformer-settings/transformer-settings.component';
+import { ObjectComponent } from './components/object/object.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransformerComponent,
+    TransformerSettingsComponent,
+    ObjectComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/transformer-settings/transformer-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transformer-settings',
+  templateUrl: './transformer-settings.component.html',
+  styleUrls: ['./transformer-settings.component.scss']
+})
+export class TransformerSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
