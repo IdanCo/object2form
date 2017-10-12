d78f13039106ecf3f9b07960e8c9b242c963e67f
Idan Cohen <idan@idancohen.com>
2017-10-12 12:37:30 +0300

    Choose Transformer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {TransformersService} from "./services/transformers.service";
+import {Transformer} from "./transformers/transformer";
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,18 @@ import {Component, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  transformers: Array<Transformer>;
+  transformer: Transformer;
+
+  constructor(private transformersService: TransformersService) {}
+
+  ngOnInit() {
+    this.transformers = this.transformersService.getTransformers();
+  }
+
+  setTransformer(transformer: Transformer) {
+    this.transformer = transformer;
+  }
+
 }
--- src/app/components/transformer/transformer.component.ts
+++ src/app/components/transformer/transformer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Transformer} from "../../transformers/transformer";
 
 @Component({
   selector: 'app-transformer',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transformer.component.scss']
 })
 export class TransformerComponent implements OnInit {
+  @Input()
+  transformers: Array<Transformer>;
+  // @Output() transformerChosen: EventEmitter<Transformer> = new EventEmitter<Transformer>();
+  @Output() transformerChosen = new EventEmitter();
+
+  chosenTransformer: Transformer;
 
   constructor() { }
 
   ngOnInit() {
+    this.chosenTransformer = this.transformers[0];
+    this.transformerChosen.emit(this.chosenTransformer);
   }
 
 }
