3bb10791e5c4c62d6f0128600f88247f4af4d0bc
Idan Cohen <idan@idancohen.com>
2017-10-14 11:49:26 +0300

    Implement Settings in Transformers

--- src/app/transformers/angular-bootstrap3.transformer.ts
+++ src/app/transformers/angular-bootstrap3.transformer.ts
@@ -4,7 +4,7 @@ export class AngularBootstrap3Transformer implements Transformer {
   name = 'Angular & Bootstrap 3';
   settings = [{
     key: 'validations',
-    label: 'add bootstrap validation classes',
+    label: 'add bootstrap 3 validation classes',
     value: true
   }];
 
@@ -20,7 +20,11 @@ export class AngularBootstrap3Transformer implements Transformer {
   }
 
   getFormControl(key, value): string {
-    return `
+    const validations = this.settings.find( e => e.key === 'validations');
+    let result;
+
+    if (validations.value) {
+      result = `
   <div class="form-group" [ngClass]="{'has-error': ${key}.invalid && ${key}.touched}">
     <label for="${key}">${key}</label>
     <input type="text" class="form-control" id="${key}"
@@ -29,6 +33,18 @@ export class AngularBootstrap3Transformer implements Transformer {
     <span class="help-block" *ngIf="${key}.invalid && ${key}.touched}">Invalid value</span>
   </div>
     `;
+    } else {
+      result = `
+  <div class="form-group">
+    <label for="${key}">${key}</label>
+    <input type="text" class="form-control" id="${key}"
+       ngModel name="${key}" #${key}="ngModel"
+       required>
+  </div>
+    `;
+    }
+
+    return result;
   }
 
 }
