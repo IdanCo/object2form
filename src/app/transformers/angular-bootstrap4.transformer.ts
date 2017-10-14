26b923cdac7718cd81d86e08ce6800dd9016ec8f
Idan Cohen <idan@idancohen.com>
2017-10-14 04:15:13 +0300

    Implement Settings in Transformers

--- src/app/transformers/angular-bootstrap4.transformer.ts
+++ src/app/transformers/angular-bootstrap4.transformer.ts
@@ -4,7 +4,7 @@ export class AngularBootstrap4Transformer implements Transformer {
   name = 'Angular & Bootstrap 4';
   settings = [{
     key: 'validations',
-    label: 'add bootstrap validation classes',
+    label: 'add bootstrap 4 validation classes',
     value: true
   }];
 
@@ -21,16 +21,62 @@ export class AngularBootstrap4Transformer implements Transformer {
   }
 
   getFormControl(key, value): string {
-    return `
+    let result;
+    console.info();
+
+    switch(typeof(value)) {
+      case 'boolean':
+        result = this.renderBooleanControl(key);
+        break;
+      default:
+        result = this.renderTextControl(key);
+    }
+
+    return result;
+  }
+
+  renderTextControl(key) {
+    let result;
+    const validations = this.settings.find( e => e.key === 'validations');
+
+    if (validations.value) {
+      result = `
   <div class="form-group">
     <label for="${key}">${key}</label>
     <input type="text" class="form-control" id="${key}"
-       ngModel name="${key}" #${key}="ngModel"
-       [ngClass]="{'is-invalid': ${key}.invalid && ${key}.touched}"
-       required>
+       ngModel name="${key}" #${key}="ngModel" required
+       [ngClass]="{'is-invalid': ${key}.invalid && ${key}.touched}">
     <div class="invalid-feedback">Invalid value</div>
   </div>
 `;
+    } else {
+      result = `
+  <div class="form-group">
+    <label for="${key}">${key}</label>
+    <input type="text" class="form-control" id="${key}"
+       ngModel name="${key}" #${key}="ngModel" required>
+  </div>
+`;
+    }
+    return result;
   }
+
+  renderBooleanControl(key) {
+    let result;
+
+    result = `
+  <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input" id="${key}"
+       ngModel name="${key}" #${key}="ngModel"
+       [ngClass]="{'is-invalid': ${key}.invalid && ${key}.touched}">
+      ${key}
+    </label>
+  </div>
+`;
+
+    return result;
+  }
+
 
 }
